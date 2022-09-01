ca5cf7b78eb72666b8d9756ef4c4e87671604667
abhibalana <abhibalana12@gmail.com>
2022-09-01 22:03:06 +0530

    First Commit UI Made

--- app/src/main/java/com/example/buffer/helper/constant.kt
+++ app/src/main/java/com/example/buffer/helper/constant.kt
@@ -7,7 +7,17 @@ class constant {
         const val host="soundcloud-scraper.p.rapidapi.com"
         const val limit=10
 
+        const val termsconditionurl =""
+        const val privacy=""
 
+        const val api_key=""
+
+        const val playlist1=""
+        const val playlist2=""
+        const val playlist3=""
+        const val playlist4=""
+        const val playlist5=""
+        const val requestIdToken =""
 
 
     }
