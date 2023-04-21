c7420b8a49258b450a1fdaf952952fcdf36d2122
Abhishek balana <68943153+abhibalana@users.noreply.github.com>
2023-04-21 06:59:31 +0530

    Update constant.kt

--- app/src/main/java/com/example/buffer/helper/constant.kt
+++ app/src/main/java/com/example/buffer/helper/constant.kt
@@ -10,9 +10,9 @@ class constant {
         const val limit=10
 
         const val termsconditionurl =
-            "https://abhibalana.github.io/TermsAndCondition/"
+            ""
         const val privacy="https://www.privacypolicygenerator.info/live.php?token=090vXmG2LNEQxC9QCbSnkXEfra1dJthH"
-        const val api_key="bb67f63ed3msh87061ca4f8b0053p1b6c46jsn210eae5a7c88"
+        const val api_key=""
 
 
         const val playlist1="https://soundcloud.com/abhishek-balana/sets/top-50-country"
@@ -20,7 +20,7 @@ class constant {
         const val playlist3="https://soundcloud.com/abhishek-balana/sets/top-50-pop"
         const val playlist4="https://soundcloud.com/abhishek-balana/sets/top-50-latin"
         const val playlist5="https://soundcloud.com/abhishek-balana/sets/top-50-audiobooks"
-        const val requestIdToken ="267797881978-ag49fktguv7dcni243jaddgh006fb397.apps.googleusercontent.com"
+        const val requestIdToken =""
         var likeSong = ArrayList<ItemsItem>()
         const val MUSIC_SERVICE_ACTION_NEXT="com.example.buffer.next"
         const val MUSIC_SERVICE_ACTION_PAUSE="com.example.buffer.pause"
